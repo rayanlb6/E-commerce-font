5bfa122f0272305173af954fcae478b500276cc8
rayanlb6 <toukaprayan6@gmail.com>
2025-02-15 19:40:50 +0100

    Allow to do make a payment that use Stripe

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -66,7 +66,7 @@
                     <div class="field">
                             <label>E-name*</label>
                             <div class="control">
-                                <input type="email" class="input" v-model="zipcode"> 
+                                <input type="email" class="input" v-model="email"> 
                             </div>
                     </div>
 
@@ -158,6 +158,13 @@
             document.title = 'Checkout | Djackets'
 
             this.cart = this.$store.state.cart
+
+            if(this.cartTotalLength > 0){
+                this.stripe = Stripe('pk_test_51QsWH2AOr0JRFx0vBA8QLEE1ifRSm1710l2vfe2rGteyS2ByKWnaqbQ7a9fQe3XafwbwB0y0kKWGi8GqLjEjnYro00yTX2Eqzf')
+                const elements = this.stripe.elements();
+                this.card = elements.create('card', { hidePostalCode: true})
+                this.card.mount('#card-element')
+            }
         },
         methods: {
             getItemTotal(item){
@@ -187,7 +194,59 @@
                 if(this.place === ''){
                     this.errors.push('The place field is missing!')
                 }
+                if(!this.errors.length){
+                    this.$store.commit('setIsLoading', true)
+
+                    this.stripe.createToken(this.card).then(result => {
+                        if(result.error){
+                            this.$store.commit('setIsLoading',false)
+
+                            this.errors.push('Something went wrong with Stripe. Please try again')
+                            console.log(result.error.message)
+                        } else {
+                            this.stripeTokenHandler(result.token)
+                        }
+                    })
+                }
+            },
+            async stripeTokenHandler(token){
+            const items=[]
+
+            for(let i=0;i<this.cart.items.length;i++){
+                const item = this.cart.items[i]
+                const obj = {
+                    product: item.product.id,
+                    quantity: item.quantity,
+                    price: item.product.price * item.quantity
+                }
+                items.push(obj)
             }
+
+            const data = {
+                'first_name':this.first_name,
+                'last_name':this.last_name,
+                'email':this.email,
+                'address':this.address,
+                'zipcode': this.zipcode,
+                'place':this.place,
+                'phone':this.phone,
+                'items':items,
+                'stripe_token': token.id
+            }
+
+            await axios
+            .post('/api/v1/checkout/', data)
+            .then(response => {
+                this.$store.commit('clearCart')
+                this.$router.push('/cart/success')
+            })
+            .catch(error => {
+                this.errors.push('Something went wrong. Please try again')
+
+                console.log(error)
+            })
+            this.$store.commit('setIsLoading',false)
+         }
         },
         computed:{
             cartTotalPrice(){
